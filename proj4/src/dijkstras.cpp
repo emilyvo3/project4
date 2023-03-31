c8a063dd431b9a14c61a2e497444f0bad8732436
Emily L Vo <evo3@hydra12.eecs.utk.edu>
2023-03-31 11:14:30 -0400

    finalized everything

--- proj4/src/dijkstras.cpp
+++ proj4/src/dijkstras.cpp
@@ -1,76 +1,77 @@
-
+//Emily Vo & Dhwani Patel
+// Project 4: Path Finding
+// CS 302
+// 3/31/23
 #include <iostream>
-#include <climits>
+#include <climits> // for INT_MAX
 #include <list>
 #include <map>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-// we need multimap for our "frontier"
-// and a list that stores the back links
-// make a separate function which only does recurssion so that we dont mess up initialization of back-links
-// so we have two functions: dij and path
-// recurssive function that will return the shortest cost
-
-void dij(vector<int> &board, vector<vector<int>> &edges, int &start, int &end, int &r){
-
-    vector<int> backlink((r * r), -1);
-    vector<int> costs((r * r), INT_MAX);
-    multimap<int, int> frontier;
+void dij(vector<int> &board, vector<vector<int>> &edges, int &start, int &end, int &r)
+{
+    vector<int> backlink((r * r), -1); // store the previous vertex in the shortest path
+    vector<int> costs((r * r), INT_MAX); // store the shortest path costs 
+    multimap<int, int> frontier; // store unvisited vertices, sorted by their cose
     multimap<int, int>::iterator it;
-    frontier.insert(make_pair(0, start));
+    frontier.insert(make_pair(0, start)); // insert the starting vertex with zeo cost into the frontier
 
     int new_dist, cost, pos;
 
-    while (!frontier.empty()){
-
+    while (!frontier.empty())
+    {
+        // select vertex with the lowest cost from the frontier
         it = frontier.begin();
         cost = it->first;
         pos = it->second;
-		
-        frontier.erase(it);
+        
+        frontier.erase(it); // remove selected vertex from the frontier
 
-
-        for (int j = 0; j < (int)edges[pos].size(); j++){
-            int curr = edges[pos][j];
-            if (curr >= 0 && curr < (int)board.size()){
-
+        for (int j = 0; j < (int)edges[pos].size(); j++)
+        {
+            int curr = edges[pos][j]; // neighbor vertex
+            if (curr >= 0 && curr < (int)board.size()) // check if the negihbor is inside the board
+            {
+                // calculate the new distance through the selected vertex to the neighbor
                 new_dist = cost + board[pos];
 
-                if (new_dist < costs[curr] ){                
+                if (new_dist < costs[curr])
+                {
+                    // update the shortest path cost and backlink for the neighbor
                     costs[curr] = new_dist;
                     backlink[curr] = pos;
 
-                    frontier.insert(make_pair(new_dist, curr)); // add the neighbor to the frontier
+                    frontier.insert(make_pair(new_dist, curr)); 
                 }
             }
         }
-
     }
-	
 
-    cout << costs[end] << endl;
+    cout << costs[end] << endl; // output the shortest/total path cost from start to end vertex
 
+    // reconstruct the shortest path from backlinks
     vector<int> path_tiles;
-    int current = end; // set the end tile as the current tile
+    int current = end; 
 
     // traverse the game board backwards from the end to start tile
     while (current != start)
     {
-        path_tiles.push_back(current); // add the current tile to the path
+        path_tiles.push_back(current); 
         current = backlink[current];   // move to the tile that led to the current tile in the shortest path
     }
 
-    path_tiles.push_back(start);                   // add the start tile to the path
+    path_tiles.push_back(start);                   
     reverse(path_tiles.begin(), path_tiles.end()); // reverse the path to obtain the correct order
 
+    /* output the tiles in the shortest path */
     for (int i = 0; i < (int)path_tiles.size(); i++)
     {
         int row = path_tiles[i] / r;
         int col = path_tiles[i] % r;
 
-            cout << row << " " << col << endl;
+        cout << row << " " << col << endl;
     }
 }
 
@@ -82,17 +83,16 @@ int main(int argc, char *argv[])
     char input;
     int value, end;
     int start = 0;
-    int c1, c2, e1, e2;
+    int c1, c2, e1, e2; // variables for inputting start and end points
 
     while (cin >> ntiles)
     {
-
         map<char, int> tiles;
         map<char, int>::iterator it;
-        end = 0;
+        end = 0; // reset end point
         board.clear();
 
-        // reading in tile names and costs
+        /* reading in tile names and costs and store them in the map */
         for (int i = 0; i < ntiles; i++)
         {
             cin >> nametile >> tcost;
@@ -102,7 +102,7 @@ int main(int argc, char *argv[])
         // reading in rows and columns
         cin >> r >> c;
 
-        // reading in board
+        /* read in the game board and store the corresponding values in the board vector */
         for (int i = 0; i < r; i++)
         {
             for (int j = 0; j < c; j++)
@@ -114,9 +114,8 @@ int main(int argc, char *argv[])
             }
         }
 
-        // reading in start(or current) and end points
+        /* read in the start and end points and calculate their positions on the board */
         cin >> c1 >> c2 >> e1 >> e2;
-
         start = (c1 * c) + c2;
         end = (e1 * c) + e2;
 
@@ -127,20 +126,19 @@ int main(int argc, char *argv[])
         edges.clear();
         index.clear();
 
+        /* loop through each element on the board and add its corresponding edges to the edges vector */
         for (int i = 0; i < r * c; i++)
         {
             x = i / r;
             y = i % r;
 
-            // checking for the 4 corners ONLY
-            // will have 2 edges
+            /* checking for the 4 corners ONLY; will have 2 edges */
             if ((y == 0 && x == 0) || (y == 0 && x == c - 1) || (y == r - 1 && x == 0) || (y == r - 1 && x == c - 1))
             {
                 index.push_back(i + 1);
                 index.push_back(i + c);
             }
-            // check the elements adjacent to the corners
-            // will have 3 edges
+            /* check the elements adjacent to the corners; will have 3 edges */
             else if ((y == 0) || (x == 0) || (y == r - 1) || (y == r - 1 && x == c - 1))
             {
                 if (y == 0)
@@ -168,8 +166,7 @@ int main(int argc, char *argv[])
                     index.push_back(i - c);
                 }
             }
-            // check for remaining elements
-            // will have 4 edges
+            /* check for remaining elements; will have 4 edges */
             else
             {
                 index.push_back(i + 1);
@@ -180,8 +177,6 @@ int main(int argc, char *argv[])
             edges.push_back(index);
             index.clear();
         }
-
-
         dij(board, edges, start, end, r);
     }
     return 0;
--- proj4/src/generate_map.cpp
+++ proj4/src/generate_map.cpp
@@ -1,112 +1,68 @@
-
-#include<iostream>
-#include<fstream>
-#include<iomanip>
-#include<random>
-#include<time.h>
+//Emily Vo & Dhwani Patel
+// Project 4: Path Finding
+// CS 302
+// 3/31/23
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <random>
+#include <time.h>
 
 using namespace std;
 
 int GetRandom(int min, int max);
 
-int main(){
-     //seeding random number generator
-	srand(time(0));
+int main()
+{
+    srand(time(0)); // seeding random number generator
 
-
-//    ifstream fin;
-    ofstream fout;
+    ofstream fout; // declare an output file stream object
     int numbers, randnum = 0, start = 0, end = 0;
-    string ofilename;
-	int ntiles = 6;
-	vector<char> tiles = {'f', 'g', 'G', 'h', 'm', 'r'};
-	vector<int> cost = {3, 1, 2, 4, 7, 5};
+    string ofilename; // declare a string variable to hold the output file name
+    int ntiles = 6;
+    vector<char> tiles = {'f', 'g', 'G', 'h', 'm', 'r'};
+    vector<int> cost = {3, 1, 2, 4, 7, 5};
 
-/*    filename = "num.txt";
-    fin.open(filename);
-    if(!fin.is_open()){
-        cout<<"Could not open file. Ceck permissions or filename.\n";
-        return 1;
-    }*/
+    cin >> numbers; // prompt the user to enter the board size or number of row/columns
 
+    ofilename = "map.txt"; // set the output file name
+    fout.open(ofilename); 
 
-    /*Create an output file to place newly formatte data */
-	
-	//10, 20, 50, 100, 200, 500, 1000
-//    while(fin>>numbers){
-        //open output file
+    fout << ntiles << endl;
 
-/*		switch(numbers){
-			case 10:
-                ofilename = "10.txt";
-                break;
-			case 20:
-                ofilename = "20.txt";
-                break;	
-            case 50:
-                ofilename = "50.txt";
-                break;
-            case 100:
-                ofilename = "100.txt";
-                break;
-            case 200:
-                ofilename = "200.txt";
-                break;
-            case 500:
-                ofilename = "500.txt";
-                break;
-            case 1000:
-                ofilename = "1000.txt";
-                break;
-        }*/
-		
-		cin>>numbers;	
+    for (int i = 0; i < (int)tiles.size(); i++)
+    {
+        fout << tiles[i] << " " << cost[i] << endl; // write the tile type and its cost to the output file
+    }
 
-		ofilename = "map.txt";
-        fout.open(ofilename);
+    fout << numbers << " " << numbers << endl; // write the number of rows/columns to the output file
 
-		fout<<ntiles<<endl;
-
-		for(int i = 0; i < (int) tiles.size(); i++){
-			fout<<tiles[i]<< " " << cost[i]<<endl;
-		}	
-
-		fout<<numbers<<" "<<numbers<<endl;
-
-        for(int i = 0; i < numbers; i++){
-            for(int j = 0; j < numbers - 1; j++){
-				randnum = GetRandom(0, (int) tiles.size() - 1);
-		        fout<<tiles[randnum]<<" ";
-			}	
-
-			randnum = GetRandom(0, (int) tiles.size() - 1);
-            fout<<tiles[randnum]<<endl;
-
-
+    for (int i = 0; i < numbers; i++) 
+    {
+        for (int j = 0; j < numbers - 1; j++) 
+        {
+            randnum = GetRandom(0, (int)tiles.size() - 1); // get a random tile type
+            fout << tiles[randnum] << " "; // write the tile type to the output file
         }
+        randnum = GetRandom(0, (int)tiles.size() - 1); // get a random tile type for the last column
+        fout << tiles[randnum] << endl; // write the tile type to the output file
+    }
 
-		start = 0;
-		end = (numbers * numbers) - 1;
-		fout<<start/numbers<<" "<<start%numbers<<endl;
-		fout<<end/numbers<<" "<<end%numbers<<endl;
+    start = 0; // set the starting point to the top-left corner
+    end = (numbers * numbers) - 1; // set the ending point to the bottom-right corner
+    fout << start / numbers << " " << start % numbers << endl; // write the starting point to the output file
+    fout << end / numbers << " " << end % numbers << endl; // write the ending point to the output fil
 
-
-        //close output file
-        fout.close();
-
-  //  }
-     //close statements
-   // fin.close();
-    
-	return 0;
+    fout.close(); 
+    return 0;
 }
 
-int GetRandom(int min, int max){
-    //Initialize random to 0 so that it gets a new value everytime we call it
+int GetRandom(int min, int max)
+{
+    // initialize random to 0 so that it gets a new value everytime we call it
     int random = 0;
 
-    //Assign a random number from 0 to 51, which are the beginning and ending index of the array 'cards'
-
+    // assign a random number from 0 to 51, which are the beginning and ending index of the array 'cards'
     random = min + rand() % (max - min + 1);
     return random;
 }
